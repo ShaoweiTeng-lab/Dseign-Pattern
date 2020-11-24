bc761f39ab5253b12646a484a3e237e0a0392d62
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2020-11-24 09:02:13 +0800

    工程師阿鄧say hi2

--- design/Assets/Assets/Script/Composite組合/control.cs
+++ design/Assets/Assets/Script/Composite組合/control.cs
@@ -14,7 +14,7 @@ namespace composite {
             UnitTest();
             // UnitTest2();
             Debug.Log("工程師say hi");
-            Debug.Log("工程師say hi2");
+            Debug.Log("工程師阿鄧say hi2");
  
             UnitTest();	
            // UnitTest2();
